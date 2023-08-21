51dc1d67a5c609c8b5b2108de2c690eea4e2cded
AilsaYang <ailsayang831031@gmail.com>
2023-08-21 18:49:01 +0800

    feat: add index.js in folder pages

---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import HomePage from "./HomePage";
+import RegisterPage from "./RegisterPage";
+import LoginPage from "./LoginPage";
+import MainPage from "./MainPage";
+import SettingPage from "./SettingPage";
+import UserSelfPage from "./UserSelfPage";
+import UserSelfFollowPage from "./UserSelfFollowPage";
+import UserOtherPage from "./UserOtherPage";
+import UserOtherFollowPage from "./UserOtherFollowPage";
+import AdminPage from "./AdminPage";
+import AdminMainPage from "./AdminMainPage";
+import AdminUsersPage from "./AdminUsersPage";
+
+
+
+
+export {
+  HomePage,
+  RegisterPage,
+  LoginPage,
+  MainPage,
+  SettingPage,
+  UserSelfPage,
+  UserSelfFollowPage,
+  UserOtherPage,
+  UserOtherFollowPage,
+  AdminPage,
+  AdminMainPage,
+  AdminUsersPage,
+};
