f4347d1efa215abc5d9bb00e62c907c586ab0be0
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-23 10:13:33 +0800

    feat: add notification box components

---
+++ src/components/NotiBoxSuccess.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SuccessIcon } from 'assets/icons'
+
+const NotiBoxSuccess = ({notiText}) => {
+
+  return (
+    <div className="notiContainer">
+      <div className="notiContent">{notiText}</div>
+      <div className="notiImgSuccess"><SuccessIcon /></div>
+    </div>
+  )
+}
+
+export default NotiBoxSuccess
