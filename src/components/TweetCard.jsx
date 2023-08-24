d35bfb686545c5d98fe10cf99bd4db00b5fad147
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-25 01:24:05 +0800

    feat: update TweetCard components

--- src/components/TweetCard.jsx
+++ src/components/TweetCard.jsx
@@ -6,19 +6,19 @@ const TweetCard = () => {
   return (
     <div className="tweetContainer">
       <div className="leftSideAvatar">
-        <img src={require('../assets/images/fakeUserOtherAvatar.png')} alt="Avatar" />
+        <img src={require("../assets/images/fakeUserOtherAvatar.png")} alt="Avatar" />
       </div>
       <div className="rightSideBox">
         <div className="upper">
-          <div className="userName">User Name</div>
+          <div className="userName">User Name1User Name1User Name1User Name1User Name1</div>
           <div className="userInfo">
-            <div className="userAccount">@user Account</div>
+            <div className="userAccount">@user userAccountrrrr</div>
             <p>．</p>
-            <div className="timer">3 小時</div>
+            <div className="timer">2 hours</div>
           </div>
-          <button className="deleteBtn"><FailIcon/></button>
+          <button className="deleteBtn"><FailIcon /></button>
         </div>
-        <div className="tweetContext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        <div className="tweetContext">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus rerum aut velit nihil eligendi in. Impedit animi dolor corrupti illo sit saepe molestiae perspiciatis perferendis explicabo inventore! Recusandae, consectetur doloribus. Velit quidem ratione repellendus, odit nemo distinctio suscipit consequuntur doloremque eveniet sint mollitia exercitationem doloribus reiciendis alias, amet quaerat blanditiis!</div>
       </div>
     </div>
   )
