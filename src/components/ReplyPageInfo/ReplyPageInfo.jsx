c81e0dc8096db354337c274fc3e3e2fd67a10c2f
AilsaYang <ailsayang831031@gmail.com>
2023-08-24 21:33:25 +0800

    feat: create ReplyPost Component

--- src/components/ReplyPageInfo/ReplyPageInfo.jsx
+++ src/components/ReplyPageInfo/ReplyPageInfo.jsx
@@ -1,27 +1,15 @@
 import "./replyPageInfo.scss";
 import { ReactComponent as BackArrowIcon } from "assets/icons/backArrowIcon.svg";
-import { ReactComponent as ReplyIcon } from "assets/icons/replyIcon.svg"
-import { ReactComponent as LikeIcon } from "assets/icons/likeIcon.svg"
+import { ReactComponent as ReplyIcon } from "assets/icons/replyIcon.svg";
+import { ReactComponent as LikeIcon } from "assets/icons/likeIcon.svg";
 
 //下面userAvatar之後要串後端
 import userAvatar from "assets/images/fakeUserAvatar.png";
 
 // 元件切分
-// const ReplyPost = ()=>{}
-// const ReplyList = () => {};
-// const ReplyItem = () => {};
-
-const ReplyPageInfo = () => {
+const ReplyPost = () => {
   return (
-    <div className="reply-page-info">
-      {/* 感覺以下可以重複使用 */}
-      <div className="header-container">
-        <BackArrowIcon className="back-arrow-icon" />
-        <h4 className="header-title">推文</h4>
-      </div>
-
-      {/* Render tweet Area */}
-
+    <div className="reply-post">
       <div className="tweet-area-wrapper">
         <div className="tweet-area-container">
           <div className="tweet-author-area">
@@ -43,7 +31,6 @@ const ReplyPageInfo = () => {
           </div>
         </div>
       </div>
-
       <div className="reply-like-area-wrapper">
         <div className="reply-like-area">
           <div className="reply-count">
@@ -56,13 +43,36 @@ const ReplyPageInfo = () => {
           </div>
         </div>
       </div>
-
       <div className="like-count-icon-area-wrapper">
         <div className="like-count-icon-area">
-            <ReplyIcon className="tweet-reply-icon" />
-            <LikeIcon className="tweet-like-icon" />
+          <ReplyIcon className="tweet-reply-icon" />
+          <LikeIcon className="tweet-like-icon" />
         </div>
       </div>
+    </div>
+  );
+};
+// const ReplyList = () => {};
+// const ReplyItem = () => {};
+
+
+
+const ReplyPageInfo = () => {
+  return (
+    <div className="reply-page-info">
+      {/* 感覺以下可以重複使用 */}
+      <div className="header-container">
+        <BackArrowIcon className="back-arrow-icon" />
+        <h4 className="header-title">推文</h4>
+      </div>
+
+      {/* Render reply Post */}
+      <ReplyPost />
+
+      <div className="reply-list">
+
+      </div>
+
     </div>
   );
 };
