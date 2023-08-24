505933dbd6aca20ee3a51931372c8d88218fac13
AilsaYang <ailsayang831031@gmail.com>
2023-08-24 20:35:38 +0800

    feat: replyPost layout

--- src/components/ReplyPageInfo/ReplyPageInfo.jsx
+++ src/components/ReplyPageInfo/ReplyPageInfo.jsx
@@ -1,8 +1,15 @@
 import "./replyPageInfo.scss";
 import { ReactComponent as BackArrowIcon } from "assets/icons/backArrowIcon.svg";
+import { ReactComponent as ReplyIcon } from "assets/icons/replyIcon.svg"
+import { ReactComponent as LikeIcon } from "assets/icons/likeIcon.svg"
 
 //下面userAvatar之後要串後端
 import userAvatar from "assets/images/fakeUserAvatar.png";
+
+// 元件切分
+// const ReplyPost = ()=>{}
+// const ReplyList = () => {};
+// const ReplyItem = () => {};
 
 const ReplyPageInfo = () => {
   return (
@@ -14,6 +21,7 @@ const ReplyPageInfo = () => {
       </div>
 
       {/* Render tweet Area */}
+
       <div className="tweet-area-wrapper">
         <div className="tweet-area-container">
           <div className="tweet-author-area">
@@ -25,11 +33,34 @@ const ReplyPageInfo = () => {
           </div>
 
           <div className="tweet-text-area">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque quisquam provident iure a cumque nulla error recusandae temporibus dolore voluptas.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
+            quisquam provident iure a cumque nulla error recusandae temporibus
+            dolore voluptas.
           </div>
+
           <div className="tweet-time-area">
             <div className="tweet-time">上午 10:05·2023年11月10日</div>
           </div>
+        </div>
+      </div>
+
+      <div className="reply-like-area-wrapper">
+        <div className="reply-like-area">
+          <div className="reply-count">
+            <div className="reply-num">34</div>
+            <div className="reply-text">回覆</div>
+          </div>
+          <div className="like-count">
+            <div className="like-num">808</div>
+            <div className="like-text">喜歡次數</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="like-count-icon-area-wrapper">
+        <div className="like-count-icon-area">
+            <ReplyIcon className="tweet-reply-icon" />
+            <LikeIcon className="tweet-like-icon" />
         </div>
       </div>
     </div>
