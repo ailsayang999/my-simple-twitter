5a0fa1bf46f3f7d2cf451becad90cf010d12f0b0
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-26 15:38:56 +0800

    refactor: remove useless function

--- src/pages/AdminUsersPage.jsx
+++ src/pages/AdminUsersPage.jsx
@@ -1,63 +1,42 @@
-import React from 'react'
-import './AdminUsersPage.scss'
-import LeftNavAdmin from 'components/LeftNavAdmin'
-import UserCard from 'components/UserCard'
-
-const dummyData = [
-    {
-        "id": 1,
-        "name": "Admin",
-        "account": "root",
-        "introduction": "Nobis harum aut quam sed eligendi rerum perspiciat",
-        "avatar": "https://loremflickr.com/320/240/man/?random=11.459194891146108",
-        "cover": "https://loremflickr.com/1440/480/city/?random=86.39469700989608",
-        "role": "admin",
-        "tweetCount":"2000",
-        "likeCount": "123455",
-        "following": "599",
-        "follower": "20"
-    },
-    {
-        "id": 52,
-        "name": "我是一隻貓",
-        "account": "cat",
-        "introduction": null,
-        "avatar": "https://picsum.photos/100/100",
-        "cover": "https://picsum.photos/id/237/700/400",
-        "role": "user",
-        "tweetCount":"2",
-        "likeCount": "5",
-        "following": "5",
-        "follower": "0"
-    },
-    {
-        "id": 50,
-        "name": "User49",
-        "account": "user49",
-        "introduction": "Incidunt sint fuga tempore quam. Veniam quo earum ",
-        "avatar": "https://loremflickr.com/320/240/man/?random=18.70303841745551",
-        "cover": "https://loremflickr.com/1440/480/city/?random=61.58283660136637",
-        "role": "user"
-    },
-    {
-        "id": 51,
-        "name": "User50",
-        "account": "user50",
-        "introduction": "Veritatis est illo praesentium consequatur.",
-        "avatar": "https://loremflickr.com/320/240/man/?random=58.14892233392464",
-        "cover": "https://loremflickr.com/1440/480/city/?random=43.96337838575563",
-        "role": "user"
-    },
-]
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './AdminUsersPage.scss';
+import LeftNavAdmin from 'components/LeftNavAdmin';
+import UserCard from 'components/UserCard';
+import { getUsers } from '../api/admin';
 
 const AdminUsersPage = () => {
+  const [users, setUsers] = useState([])
+  const navigate = useNavigate();
+  const { isAuthenticated, currentMember } = 
+
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const users = await getUsers();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getUsers();
+  }, [])
+
+  //若未登入跳轉回"後台登入"頁面
+  useEffect(() => {
+    if(!isAuthenticated) {
+      Navigate('/admin')
+    }
+  }, [navigate, isAuthenticated]);
+
+
   return (
     <div className="adminContainer">
       <LeftNavAdmin/>
       <div className="rightContainer">
         <div className="title">使用者列表</div>
         <div className="tweetBoard">
-          {dummyData.map((user) => {
+          {users.map((user) => {
             return (<UserCard className="userCard"
             key={user.id}
             id={user.id}
