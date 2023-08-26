c262ed61297922781d2bf7bf1317a9686f0a1ecd
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-26 11:44:33 +0800

    feat: add AdminUsersPage-layout

--- src/pages/AdminUsersPage.jsx
+++ src/pages/AdminUsersPage.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import './AdminUsersPage.scss'
+import LeftNavAdmin from 'components/LeftNavAdmin'
+import UserCard from 'components/UserCard'
 
 const AdminUsersPage = () => {
   return (
-    <div>AdminUsersPage</div>
+    <div className="adminContainer">
+      <LeftNavAdmin />
+      <div className="rightContainer">
+        <div className="title">使用者列表</div>
+        <div className="tweetBoard">
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" /> 
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+          <UserCard className="userCard" />
+        </div>
+      </div>
+    </div>
   )
 }
 
