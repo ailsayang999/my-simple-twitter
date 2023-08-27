e39f24e6e1d7fc5d7fe86345dcbcd7e2a45b0c6f
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-28 01:52:27 +0800

    feat: add AuthProvider and AuthAdminProvider

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,8 @@ import {
   AdminMainPage,
   AdminUsersPage,
 } from "pages";
-import { AuthAdminProvider } from 'context/AdminAuthContext'
+import { AuthProvider } from "context/AuthContext";
+import { AuthAdminProvider } from 'context/AdminAuthContext';
 
 
 function App() {
@@ -23,21 +24,24 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <AuthAdminProvider>
-          <Routes>
-            <Route path="*" element={<HomePage />} />
-            <Route path="register" element={<RegisterPage />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="main" element={<MainPage />} />
-            <Route path="reply" element={<ReplyPage />} />
-            <Route path="setting" element={<SettingPage />} />
-            <Route path="user/self" element={<UserSelfPage />} />
-            <Route path="user/self/follow" element={<UserSelfFollowPage />} />
-            <Route path="user/other" element={<UserOtherPage />} />
-            <Route path="user/other/follow" element={<UserOtherFollowPage />} />
-            <Route path="admin" element={<AdminPage />} />
-            <Route path="admin_main" element={<AdminMainPage />} />
-            <Route path="admin_users" element={<AdminUsersPage />} />
-          </Routes>
+          <AuthProvider>
+            <Routes>
+              <Route path="*" element={<HomePage />} />
+              <Route path="register" element={<RegisterPage />} />
+              <Route path="login" element={<LoginPage />} />
+              <Route path="reply" element={<ReplyPage />} />
+              <Route path="main" element={<MainPage />} />
+              
+              <Route path="setting" element={<SettingPage />} />
+              <Route path="user/self" element={<UserSelfPage />} />
+              <Route path="user/self/follow" element={<UserSelfFollowPage />} />
+              <Route path="user/other" element={<UserOtherPage />} />
+              <Route path="user/other/follow" element={<UserOtherFollowPage />} />
+              <Route path="admin" element={<AdminPage />} />
+              <Route path="admin_main" element={<AdminMainPage />} />
+              <Route path="admin_users" element={<AdminUsersPage />} />
+            </Routes>
+          </AuthProvider>
         </AuthAdminProvider>
       </BrowserRouter>
     </div>
