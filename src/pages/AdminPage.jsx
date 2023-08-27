e5ee3e7b7b78428f53a15ad57044c498a09dc024
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-28 01:59:39 +0800

    temp: add adminPage login function (not finished)

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -4,26 +4,63 @@ import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Header from 'components/Header'
 import InputSet from 'components/InputSet'
+import { useAdminAuth } from 'context/AdminAuthContext';
+import NotiBoxSuccess  from '../components/NotiBoxSuccess';
+import NotiBoxFail from '../components/NotiBoxFail';
 
 const AdminPage = () => {
-  const [username, setUsername] = useState('');
+  const [account, setAccount] = useState('');
   const [password, setPassword] = useState('');
+  const [showNotiBox, setShowNotiBox] = useState(false);
+  const [loginSuccess, setLoginSuccess] = useState(true);
+  const navigate = useNavigate();
 
-  function handleClick() {
-    console.log('後台登入')
+  const { adminLogin, isAuthenticated} = useAdminAuth();
+
+  const handleClick = async() => {
+    console.log("點擊登入")
+    if (account.length === 0) {
+      return;
+    }
+    if (password.length === 0) {
+      return;
+    }
+    console.log("資料通過長度檢驗，進入await")
+    const {success} = await adminLogin({
+      account,
+      password
+    });
+    if (success) {
+      // localStorage.setItem('authToken', authToken);
+      setShowNotiBox(true)
+      setLoginSuccess(true)
+      console.log('登入成功!')
+    } else {
+      setShowNotiBox(true)
+      setLoginSuccess(false)
+      console.log('登入失敗!')
+    }
   }
 
+  // const handleClick = () => {
+  //   setShowNotiBox(true);
+  //   setLoginSuccess(false)
+  // } 
+  useEffect(() => {
+  if (isAuthenticated) {
+    navigate('/admin_main');
+  }
+  }, [navigate, isAuthenticated]);
 
   return (
     <div className="outerContainer">
-      <div className="notificationBox"></div>
+      <div className={(showNotiBox)? "show" : "hide"}>{(loginSuccess)? <NotiBoxSuccess notiText={"登入成功!"}/> : <NotiBoxFail notiText={"登入失敗!"}/>}</div>
       <Header entryName={"後台登入"}/>
       <InputSet 
         label={"帳號"} 
         placeholder={"請輸入帳號"} 
-        value={username}
-        onChange={(nameInputValue) => setUsername(nameInputValue)}
-        // errorMsg={errorMsg}
+        value={account}
+        onChange={(nameInputValue) => setAccount(nameInputValue)}
         />  
       <InputSet 
         label={"密碼"} 
@@ -46,3 +83,9 @@ const AdminPage = () => {
 }
 
 export default AdminPage
+
+
+// 管理者可從專門的後台登入頁面進入網站後台
+
+// 管理者帳號不可登入前台，詳見【角色權限】單元說明
+// 若使用管理帳號登入前台，或使用一般使用者帳號登入後台，等同於「帳號不存在」
