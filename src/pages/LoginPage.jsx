9955ddcd0d7c1c3108f7a152f8da83c014749931
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-28 02:00:26 +0800

    temp: add loginPage function(not finished)

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,56 +2,45 @@ import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Header from 'components/Header'
 import InputSet from 'components/InputSet'
-import NotiBoxSuccess from 'components/NotiBoxSuccess' //請自行輸入notiText - 登入成功! 
-import NotiBoxFail from 'components/NotiBoxFail' //請自行輸入notiText - 登入失敗! / 帳號不存在!
+// import NotiBoxSuccess from 'components/NotiBoxSuccess' //請自行輸入notiText - 登入成功! 
+// import NotiBoxFail from 'components/NotiBoxFail' //請自行輸入notiText - 登入失敗! / 帳號不存在!
+import { useAuth } from 'context/AuthContext';
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
+  const [account, setAccount] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleClick() {
-    console.log('登入')
-  }
-  // const navigate = useNavigate();
+  // function handleClick() {
+  //   console.log('登入')
+  // }
+  const navigate = useNavigate();
 
-  // const { login, isAuthenticated } = useAuth(); // 取出需要的狀態與方法
+  const { login, isAuthenticated } = useAuth(); // 取出需要的狀態與方法
 
-  // const handleClick = async () => {
-  //   if (username.length === 0) {
-  //     return;
-  //   }
-  //   if (password.length === 0) {
-  //     return;
-  //   }
+  const handleClick = async () => {
+    if (account.length === 0) {
+      return;
+    }
+    if (password.length === 0) {
+      return;
+    }
 
-  //   const { success } = await login({
-  //     username,
-  //     password,
-  //   });
-  //   if (success) {
-  //     // localStorage.setItem('authToken', authToken);
-  //     Swal.fire({
-  //       title: '登入成功',
-  //       icon: 'success',
-  //       showConfirmButton: false,
-  //       time: 1000,
-  //       position: 'top',
-  //     });
-  //     // navigate('/todo');
-  //     return;
-  //   }
-  //   Swal.fire({
-  //     title: '登入失敗',
-  //     icon: 'error',
-  //     showConfirmButton: false,
-  //     time: 1000,
-  //     position: 'top',
-  //   });
-  // };
+    const { success } = await login({
+      account,
+      password,
+    });
+    if (success) {
+      // localStorage.setItem('authToken', authToken);
+      console.log('登入成功!')
+      // navigate('/todo');
+      return;
+    }
+    console.log('登入失敗!')
+  };
 
   // useEffect(() => {
   //   if (isAuthenticated) {
-  //     navigate('/todo');
+  //     navigate('/main');
   //   }
   // }, [navigate, isAuthenticated]);
 
@@ -62,8 +51,8 @@ const LoginPage = () => {
       <InputSet 
         label={"帳號"} 
         placeholder={"請輸入帳號"} 
-        value={username}
-        onChange={(nameInputValue) => setUsername(nameInputValue)}
+        value={account}
+        onChange={(accountInputValue) => setAccount(accountInputValue)}
         // errorMsg={errorMsg}
         />  
       <InputSet 
