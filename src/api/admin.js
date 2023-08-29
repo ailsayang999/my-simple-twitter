d1b1c8a3252468326a9bae5d70b687983a728900
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-30 00:22:05 +0800

    fix: remove checkPermission func

--- src/api/admin.js
+++ src/api/admin.js
@@ -12,47 +12,48 @@ export const adminLogin = async ({ account, password }) => {
       password,
     });
 
-    console.log(`data:${JSON.stringify(data)}`)
-    // const authToken = data.data.token
-    // console.log(`取出data中的token值，改命名為authToken:${authToken}`)
+    console.log(`登入後台從後端signin拿回來的data:${JSON.stringify(data)}`)
+    const authToken = data.data.token
 
-    // if (authToken) {
-    //   console.log('AuthToken get! Add "success": true, "authToken": authToken  into data(最外層)!')
-    //   return { success: true, authToken: authToken, ...data };
-    // }
-    // console.log(`auth.js中的login通過authToken驗證 success 後拿到data做JSON.stringify後的data: ${JSON.stringify(data)}`)
-    // return data;
+    console.log(`取出data中的token值，改命名為authToken:${authToken}`)
+    
+
+    if (authToken) {
+      localStorage.setItem('authToken', authToken);
+      console.log('將authToken存入localStorage Add "success": true, "authToken": authToken  into data(最外層)!')
+      return { "success": true, "authToken": authToken, ...data };
+    }
+    console.log(`auth.js中的login通過authToken驗證 success 後拿到data做JSON.stringify後的data: ${JSON.stringify(data)}`)
+    return data;
   } catch (error) {
     console.error(`[AdminLogin Failed]:`, error);
   }
 };
 
-export const checkPermission = async (authToken) => {
-  try {
-    const response = await axios.get(`${baseUrl}/test-token`, {
-      headers: {
-        Authorization: 'Bearer ' + authToken,
-      },
-    });
-    return response.data.success;
-  } catch (error) {
-    console.error(`[Check Permission Failed]:`, error);
-  }
-};
+//先不使用checkPermission  (沒有另外做一支test-token的api)
+// export const checkPermission = async (authToken) => {
+//   try {
+//     const response = await axios.get(`${baseUrl}/test-token`, {
+//       headers: {
+//         Authorization: 'Bearer ' + authToken,
+//       },
+//     });
+//     return response.data.success;
+//   } catch (error) {
+//     console.error(`[Check Permission Failed]:`, error);
+//   }
+// };
 
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
 });
 
-// 直接從後台拿authToken: "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWNjb3VudCI6InJvb3QiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTI5NTgxMjQsImV4cCI6MTY5NTU1MDEyNH0.VJeixMkMSaXJioVrgDFfm-izWdDLwYxzq3aMicCC8v8"
-
-
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    // const token = localStorage.getItem('authToken');
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWNjb3VudCI6InJvb3QiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTI5NTgxMjQsImV4cCI6MTY5NTU1MDEyNH0.VJeixMkMSaXJioVrgDFfm-izWdDLwYxzq3aMicCC8v8"
+    const token = localStorage.getItem('authToken');
+    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWNjb3VudCI6InJvb3QiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTI5NTgxMjQsImV4cCI6MTY5NTU1MDEyNH0.VJeixMkMSaXJioVrgDFfm-izWdDLwYxzq3aMicCC8v8"
 
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
