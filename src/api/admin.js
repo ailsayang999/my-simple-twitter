1d23c22bcb555eef69c405e0e0fc72454a0959c5
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-31 03:19:22 +0800

    feat: add API data port

--- src/api/admin.js
+++ src/api/admin.js
@@ -6,26 +6,25 @@ const baseUrl = 'https://mighty-temple-45104-0d6672fb07d0.herokuapp.com/api/admi
 export const adminLogin = async ({ account, password }) => {
   try {
     console.log(`進到admin.js 傳入資料,account:${account} password:${password}`)
-    const { data } = await axios.post(`${baseUrl}/login`, {
+    const {data}  = await axios.post(`${baseUrl}/signin`, {
       account,
       password,
     });
 
 
     console.log(`登入後台從後端signin拿回來的data:${JSON.stringify(data)}`)
+    console.log(`登入後台從後端signin拿回來的authToken:${data.data.token}`)
     const authToken = data.data.token
-
-    console.log(`取出data中的token值，改命名為authToken:${authToken}`)
-    
 
     if (authToken) {
       localStorage.setItem('authToken', authToken);
       console.log('將authToken存入localStorage Add "success": true, "authToken": authToken  into data(最外層)!')
-      return { "success": true, "authToken": authToken, ...data };
-
+     
+      return { "authToken": authToken, ...data };
+    } else {
+      console.log("AuthToken missed! Find one!")
+      return data;
     }
-    console.log(`auth.js中的login通過authToken驗證 success 後拿到data做JSON.stringify後的data: ${JSON.stringify(data)}`)
-    return data;
   } catch (error) {
     console.error(`[AdminLogin Failed]:`, error);
   }
@@ -57,8 +56,6 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('authToken');
-    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWNjb3VudCI6InJvb3QiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTI5NTgxMjQsImV4cCI6MTY5NTU1MDEyNH0.VJeixMkMSaXJioVrgDFfm-izWdDLwYxzq3aMicCC8v8"
-
 
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
@@ -74,9 +71,7 @@ axiosInstance.interceptors.request.use(
 export const getUsers = async () => {
   try {
     const res = await axiosInstance.get(`${baseUrl}/users`);
-    console.log(res.data)
-    console.log(res.data.data.id)
-    return res.data.data;
+    return res.data;
   } catch (error) {
     console.error(`[Get Users failed]:`, error);
   }
@@ -85,8 +80,6 @@ export const getUsers = async () => {
 export const getTweets = async () => {
   try {
     const res = await axiosInstance.get(`${baseUrl}/tweets`);
-    console.log(res.data)
-    console.log(res.data.id)
     return res.data;
   } catch (error) {
     console.error(`[Get Tweets failed]:`, error);
@@ -96,9 +89,7 @@ export const getTweets = async () => {
 export const deleteTweet = async (id) => {
   try {
     const res = await axiosInstance.del(`${baseUrl}/tweets/:${id}`);
-    console.log(res.data)
-    console.log(res.data.data.id)
-    return res.data.data;
+    return res.data;
   } catch (error) {
     console.error(`[Delete Tweet failed]:`, error);
   }
