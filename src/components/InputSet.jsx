300dc91ed0fd047bb62a878cea916cd7cf74ea57
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-09-03 01:52:39 +0800

    doc: add notes for input field limitation

--- src/components/InputSet.jsx
+++ src/components/InputSet.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+// import { useState } from 'react'
 import './CommonStyle.scss'
 
 
 const InputSet = ({ item, type, label, value, placeholder, maxLength, onChange }) => {
   let errorType = "";
   let errorMsg = "";
-
+  // const [inputValue, setInputValue] = useState(value || '');
+  // const [isValid, setIsValid] = useState(true);
 
   // 各種 error 判斷條件產出errorType 及對應的 errorMsg
   if(label === "帳號" && (value.length > 20)) {
@@ -22,7 +24,7 @@ const InputSet = ({ item, type, label, value, placeholder, maxLength, onChange }
     errorMsg = "密碼長度至少5字，但不超過20字!";
   } 
 
-
+  //根據不同 errorType 改變底線顏色
   const BottomLine = ({errorType}) => {
     if (errorType === "error") {
       return(<div className="inputBottomLineError"></div>)
@@ -33,6 +35,23 @@ const InputSet = ({ item, type, label, value, placeholder, maxLength, onChange }
     }
   }
 
+  // const HandleChange = (e) => {
+  //   const newValue = e.target.value;
+  //   setInputValue(newValue);
+
+  //   const pattern = /^[0-9a-zA-Z]+$/;
+  //   const isInputValid = pattern.test(newValue);
+  //   setIsValid(isInputValid);
+
+  //   if (isInputValid) {
+  //     console.log("Valid Input!");
+  //     if (onChange) {
+  //       onChange(newValue); // 調用外部的 onChange 回調函數
+  //     }
+  //   } else {
+  //     console.log("Invalid Input!");
+  //   }
+  // };
 
   return (
     <div className="inputContainer">
