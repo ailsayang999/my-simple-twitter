df3b2016b45241dc80f1a8f70c0f300702b25811
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-09-04 03:06:00 +0800

    fix: change eng unit into Chinese unit

--- src/components/TweetCard.jsx
+++ src/components/TweetCard.jsx
@@ -23,13 +23,13 @@ const TweetCard = ({authorId, tweetId, avatar, name, account, description, time}
     const yearsDifference = Math.floor(timeDifference / (365 * 24 * 60 * 60 * 1000));
 
     if (minsDifference < 60) {
-      let newTime = `${Math.floor(timeDifference / (60 * 1000))} min`
+      let newTime = `${Math.floor(timeDifference / (60 * 1000))} 分`
       return newTime
     } else if (daysDifference < 1) {
-      let newTime = `${Math.floor(timeDifference / (60 * 60 * 1000))} h`
+      let newTime = `${Math.floor(timeDifference / (60 * 60 * 1000))} 時`
       return newTime
     } else if (daysDifference < 30) {
-      let newTime = `${daysDifference} d`;
+      let newTime = `${daysDifference} 天`;
       return newTime
     } else if (daysDifference < 365) {
       const month = String(createdAtDate.getMonth() + 1).padStart(2, '0');
@@ -37,7 +37,7 @@ const TweetCard = ({authorId, tweetId, avatar, name, account, description, time}
       let newTime = `${month}-${day}`;
       return newTime
     } else {
-      let newTime = `${yearsDifference} y`;
+      let newTime = `${yearsDifference} 年`;
       return newTime
     }
   }
